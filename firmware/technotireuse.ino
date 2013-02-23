cb4fc906962eb3e45a62c457653becdb45306ce3
alex <alex@alex-ThinkPad-X61.(none)>
2013-02-23 21:14:42 +0100

    new code integrated

--- firmware/technotireuse.ino
+++ firmware/technotireuse.ino
@@ -1,5 +1,13 @@
 #include <SoftwareSerial.h>
+#include <Servo.h> 
 
+Servo myServo;
+int mot=7;
+int interuptOpen=2;//open
+int interuptClose=3;
+int servoPin=6;
+int tempoOpen=6300; //time needed to fill the glass
+int tempoDrops=1000; //for the last drops -> faut pas gacher
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 unsigned char req_command = 29;
@@ -7,7 +15,11 @@ int last_coin = 0;
 int total_value = 0;
 
 void setup() 
-{ 
+{
+   pinMode(mot,OUTPUT);
+   digitalWrite(mot, LOW);
+   myServo.attach(servoPin);
+   myServo.write(10);
    mySerial.begin(9600);
    mySerial.listen();
    Serial.begin(9600);
@@ -45,6 +57,22 @@ void loop() {
     }
     //
     Serial.println("Purchase OK!");
-    
+      digitalWrite(mot,HIGH);
+    while(digitalRead(interuptOpen)==HIGH){
+    //delay(20); ?
+    }
+    delay(tempoOpen);
+    digitalWrite(mot,LOW);
+    while(digitalRead(interuptClose)==HIGH){
+      //delay(20); ?
+    }
+    delay(tempoDrops);
+    int i;
+    for(i=40;i<=120;i=i+20){
+      myServo.write(i);
+      delay(100);
+    }
+    delay(50);
+    myServo.write(20);
     total_value = 0;
 } 
