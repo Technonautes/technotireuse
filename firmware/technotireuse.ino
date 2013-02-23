b58a9c15fb3cbfa9118d55708afbc8fdf842c2d9
Gabriel Dulac-Arnold <gabe@squirrelsoup.net>
2013-02-23 22:03:27 +0100

    Needs pin configurations.

--- firmware/technotireuse.ino
+++ firmware/technotireuse.ino
@@ -29,14 +29,15 @@ void loop() {
 	while (total_value < 250) {
 		mySerial.write((byte)0); //INIT
 		mySerial.write(req_command); //0x1D Request
-		delay(15);
+		delay(15); //Give things time to run out
 
 		//Throw away these two bytes:
 		mySerial.read();
 		mySerial.read();
 
+		//Wait until the RM5 responds
 		while (!mySerial.available()) {}
-		delay(10);
+		delay(10); //Give enough time for all messages to arrive
 		last_coin = mySerial.read() << 8;
 		last_coin += mySerial.read() & 0xFF;
 
@@ -51,28 +52,41 @@ void loop() {
 				total_value += 100;
 				break;
 		}
-
 		Serial.println(total_value);
 	    delay(1000);
     }
     //
     Serial.println("Purchase OK!");
-      digitalWrite(mot,HIGH);
+    
+    Serial.println("Starting tap motor (1st).");
+    digitalWrite(mot,HIGH); //Turn on tap motor
+    //Wait until tap is fully opened (1st button closed)
     while(digitalRead(interuptOpen)==HIGH){
     //delay(20); ?
     }
+    Serial.println("Stopping tap motor (1st).");
+    digitalWrite(mot,LOW); //Turn the motor  off
     delay(tempoOpen);
-    digitalWrite(mot,LOW);
+    Serial.println("Starting tap motor (2nd).");
+    digitalWrite(mot,HIGH); //Turn the motor back on
+    //Wait until tap is fully closed (2nd button closed)
     while(digitalRead(interuptClose)==HIGH){
       //delay(20); ?
     }
-    delay(tempoDrops);
+    Serial.println("Stopping tap motor (2nd).");
+    digitalWrite(mot,LOW); //Turn the motor  off
+
+    delay(tempoDrops); //Give the beer some time to drip...
+    //Push the cup gradually with the servo
+    Serial.println("Push cup out of way.");
     int i;
     for(i=40;i<=120;i=i+20){
       myServo.write(i);
       delay(100);
     }
     delay(50);
-    myServo.write(20);
-    total_value = 0;
+    Serial.println("Bring servo back.");
+    myServo.write(20); //Bring servo back in position
+    Serial.println("Done!");
+    total_value = 0; //Reset the money counter to zero for next client
 } 
